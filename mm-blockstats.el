00ea1749448ce9abde9f5954e61adfaade66848b
JeremyRand <biolizard89@gmail.com>
2018-03-08 00:17:30 +0000

    Add BitClub Network.

--- mm-blockstats.el
+++ mm-blockstats.el
@@ -267,6 +267,8 @@
                 (setq loc-resultsymbol (intern (concat "BTC.com-" (format "%05x" loc-coinbaseversion)))))
                ((string-match "/BTC.TOP/" loc-resultstring)
                 (setq loc-resultsymbol (intern (concat "BTC.TOP-" (format "%05x" loc-coinbaseversion)))))
+               ((string-match "/BitClub Network/" loc-resultstring)
+                (setq loc-resultsymbol (intern (concat "BitClub Network-" (format "%05x" loc-coinbaseversion)))))
 	       ((string-match "P2SH" loc-resultstring)
 		(setq loc-resultsymbol (intern (concat "unknown-P2SH-supporter-" (format "%05x" loc-coinbaseversion)))))
 	       (t
